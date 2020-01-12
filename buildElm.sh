88666b316a8e3c941e20de7f7b941d98d86ac64e
Max Bussiere <3digitdev@gmail.com>
2020-01-12 12:43:58 -0600

    oops

--- buildElm.sh
+++ buildElm.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Standard building
 # Compile Elm
-# elm make src/Main.elm --optimize --output=public/index.js
+elm make src/Main.elm --optimize --output=public/index.js
 # Compile SASS/LESS/CSS
 sass --no-cache --sourcemap=none --update public/css/main.sass:public/css/main.css
 
 
 # Debug Commands
-elm make src/Main.elm --output=public/index.js
-elm reactor
+# elm make src/Main.elm --output=public/index.js
+# elm reactor
